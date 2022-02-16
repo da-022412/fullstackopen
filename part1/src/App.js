f935bd324217ce4ff7971bc45bfc3687f7e79d62
Dennis Acosta <email@dennisacosta.me>
2022-02-15 21:44:00 -0500

    EOD commit

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,20 +1,26 @@
 import React from 'react';
-const Hello = (props) => {
+const Hello = ({ name, age }) => {
+    const bornYear = () => new Date().getFullYear() - age;
+
     return (
         <div>
-            Hello {props.name}, you are {props.age} years old
+            <p>
+                Hello {name}, you are {age} years old
+            </p>
+            <p>So you were probably born in {bornYear()}</p>
         </div>
     );
 };
 
 const App = () => {
-    const name = 'Dennis';
-    const age = 35;
+    const name = 'Peter';
+    const age = 10;
 
     return (
         <div>
             <h1>Greetings</h1>
-            <Hello name={name} age={age + 10} />
+            <Hello name='Maya' age={26 + 10} />
+            <Hello name={name} age={age} />
         </div>
     );
 };
