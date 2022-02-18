e2d223ebfacc15fbe3c85850bf88c0b37b6b0a77
Dennis Acosta <email@dennisacosta.me>
2022-02-18 18:35:01 -0500

    Start next section

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,33 @@
 import { useState } from 'react';
 
-const Display = ({ counter }) => {
-    return <div>{counter}</div>;
-};
-
-const Button = ({ onClick, text }) => {
-    return <button onClick={onClick}>{text}</button>;
-};
-
 const App = () => {
-    const [counter, setCounter] = useState(0);
+    const [clicks, setClicks] = useState({
+        left: 0,
+        right: 0,
+    });
 
-    const plusOne = () => setCounter(counter + 1);
-    const minusOne = () => setCounter(counter - 1);
-    const toZero = () => setCounter(0);
+    const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right,
+        };
+        setClicks(newClicks);
+    };
+
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1,
+        };
+        setClicks(newClicks);
+    };
 
     return (
         <div>
-            <Display counter={counter} />
-            <Button onClick={plusOne} text='plus' />
-            <Button onClick={toZero} text='zero' />
-            <Button onClick={minusOne} text='minus' />
+            {clicks.left}
+            <button onClick={handleLeftClick}>left</button>
+            <button onClick={handleRightClick}>right</button>
+            {clicks.right}
         </div>
     );
 };
